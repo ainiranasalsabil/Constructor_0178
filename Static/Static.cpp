2c910d2270a6676e899045eef76b412e71cbd220
ainiranasalsabil <ainiranasalsabil04@gmail.com>
2024-05-14 13:45:51 +0700

    membuat class mahasiswa yang bersifat public yang berisi variabel nim,id,nama

---
+++ Static/Static.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	Mahasiswa(string pnama) :nama(pnama) {
+		setID();
+	}
+};
+
